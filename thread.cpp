352905657956c5f23b5d49f7367af5e0dfcd1734
oraarthornsombat <oa3cj@virginia.edu>
2017-03-21 15:28:49 -0600

    added some commented out code for switching arrays

--- thread.cpp
+++ thread.cpp
@@ -14,14 +14,16 @@ or gcc thread.cpp -std=c++11 -lpthread -lstdc++ */
 
 /********** GLOBAL VARIABLES ***********/
 int arr [4096];
-int newarr[4096];
+int newarr[4096] = {0};
 int arrcount =0;
 int i=0;
 int maxnum=0;
 int number_of_threads;
+bool array1zero = false;
+bool array2zero = true;
 std::mutex mtx; // mutex for critical section
 std::condition_variable cv; //condition variable for critical section
-int current = 0; //current count to compare with condition variable
+int current_count = 0; //current count to compare with condition variable
 
 //This function will be called from a thread
 void call_from_thread(int thread_id) {
@@ -29,23 +31,75 @@ void call_from_thread(int thread_id) {
     int index=0;
     cout << "Launched by thread "<< thread_id << "\n";
  
-    for (i = 0; i< arrcount ; i+=2){
-        maxnum = 0;
-        if (i == thread_id * 2){
-            //cout << i << "thread: "<< thread_id * 2<< endl;
-            if (arr[i] > arr[i+1]){
-                maxnum = arr[i];
-            } else {
-                maxnum = arr[i+1];
+    if (array1zero == false){
+        //array 1 (arr) has the values. write result of comparison to array 2 (newarr).
+        for (i = 0; i< arrcount ; i+=2){
+            maxnum = 0;
+            /*figure out which index to look at (thread 0 at index 0, thread 1 at index 2, 
+            thread 2 at index 4, etc.) */
+            if (i == thread_id * 2){
+                //cout << i << "thread: "<< thread_id * 2<< endl;
+                if (arr[i] > arr[i+1]){
+                    maxnum = arr[i];
+                } else {
+                    maxnum = arr[i+1];
+                }
+                newarr[index] = maxnum;
             }
-            //cout << maxnum;
-            newarr[index] = maxnum;
+
+            index++;
         }
-        index++;
+        // if (thread_id ==0){
+        //     //last thread of the current run to come through
+        //     for (i=0; i<arrcount;i++){
+        //         //rewrite whole arr to be zeroes, to use for storing next round of comparisons
+        //         arr[i] = 0;
+        //     }
+        //     //now new_arr has the values, and arr is all zeroes
+        //     array1zero = true;
+        //     array2zero = false;
+        // }
+
+
+    } else if (array1zero == true) {
+        // array 2 (newarr) has the values. write result of comparison to array 1 (arr).
+         for (i = 0; i< arrcount ; i+=2){
+            maxnum = 0;
+            /*figure out which index to look at (thread 0 at index 0, thread 1 at index 2, 
+            thread 2 at index 4, etc.) */
+            if (i == thread_id * 2){
+                //cout << i << "thread: "<< thread_id * 2<< endl;
+                if (newarr[i] > newarr[i+1]){
+                    maxnum = newarr[i];
+                } else {
+                    maxnum = newarr[i+1];
+                }
+                arr[index] = maxnum;
+            }
+
+            index++;
+        }
+        // if (thread_id ==0){
+        //     //last thread of the current run to come through
+        //     for (i=0; i<arrcount;i++){
+        //     //rewrite whole new_arr to be zeroes, to use for storing next round of comparisons
+        //         newarr[i] = 0;
+        //     }
+        //     //now arr has the values, and new_arr is all zeroes
+        //     array1zero = false;
+        //     array2zero = true;
+        // }
     }
+
+
     for (i=0; i<number_of_threads; i++){
         cout << newarr[i] << endl;
+       // cout << "arr2 should be 0: "<<array2zero << endl;
     }
+    // for (i=0; i<arrcount;i++){
+    //     cout << arr[i] << "should be 0 for 1st run" << endl;
+    //    // cout << "arr should be 1: "<<array1zero << endl;
+    // }
     
 }
 
@@ -58,7 +112,6 @@ int main(int argc, char *argv[]) {
         cout<<"Please enter a file name as well \n";
         return -1;
     }
-
     string line;
     ifstream myfile (argv[1]);
     if (myfile.is_open()){
@@ -69,8 +122,6 @@ int main(int argc, char *argv[]) {
           arrcount = i;
         }
         myfile.close();
-        //number_of_threads = arrcount/2;
-        //cout << number_of_threads;
     }
 
     else {
@@ -88,6 +139,8 @@ int main(int argc, char *argv[]) {
     //Launch a group of threads
     for (int i = 0; i < number_of_threads; ++i) {
         t[i] = std::thread(call_from_thread, i);
+        //  cout << "arr is zero? " << array1zero << endl;
+        // cout << "new_arr is zero? " << array2zero << endl;
 
     }
 
